cc8b3ff9028735a68555d2839157f2b1d368ddf5
Lavanya <lavaabi@gmail.com>
2025-08-07 03:15:52 -0700

    session name changes

--- app/Filters/Authentication.php
+++ app/Filters/Authentication.php
@@ -10,17 +10,18 @@ class Authentication implements FilterInterface
 {
 
 	public function before(RequestInterface $request, $arguments = null)
-	{
-		
-		if (!session()->get('vr_sess_logged_in') && !session()->get('admin_sess_logged_in')) {
-			$currentPath = $request->getPath(); // e.g., "admin/dashboard"
+	{		
+		$currentPath = $request->getPath(); // e.g., "admin/dashboard"
 
-			if (str_starts_with($currentPath, '/admin')) {
+		if (str_starts_with($currentPath, '/admin')) {
+			if (!session()->get('admin_sess_logged_in')) {
 				return redirect()->to(base_url('auth/login'));
-			} else {
+			}
+		} else {
+			if (!session()->get('vr_sess_logged_in')) {
 				return redirect()->to(base_url('login'));
 			}
-		}
+		}		
 		
 	}
 	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)

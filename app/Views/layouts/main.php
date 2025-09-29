f71ca7bc0fda2f71c48d1993ab56bbd9ecb7791a
Lavanya <lavaabi@gmail.com>
2025-09-28 21:09:33 -0700

    Google Tag Manager

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -120,20 +120,13 @@
     </script>
     <script src="<?php echo base_url(); ?>/assets/frontend/js/custom-frontend.js?version=2.0"></script>
     
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T7PMX5MS');</script>
-<!-- End Google Tag Manager -->
+     <!-- Google tag (gtag.js) --> 
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-BMHFJMLGQL"></script> 
+  <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-BMHFJMLGQL'); </script>
 </head>
 <?php $uri = current_url(true);?>
 <body class="hold-transition <?php echo (session()->get('vr_sess_logged_in') == TRUE) ? ' logged-in ' : ''; ?> <?php echo (base_url() == str_replace('/index.php/','',$uri)) ? 'home' : ''; ?> " >
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7PMX5MS"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
     <!-- HEADER: MENU + HEROE SECTION -->
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
   <symbol id="bootstrap" viewBox="0 0 118 94">
